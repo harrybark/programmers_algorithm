77407d310807dfb82634c1cee70252739c943568
harrybark <harrypmw@gmail.com>
2021-11-24 21:49:00 +0900

    2021.11.24 adding Programmers Algorithm

--- src/programmers/level/level1/AddProcession.java
+++ src/programmers/level/level1/AddProcession.java
@@ -13,20 +13,37 @@ import java.util.Arrays;
  * [[1],[2]]	    [[3],[4]]	    [[4],[6]]
  */
 
+
 public class AddProcession {
 
     private static int[][] arr1 = {{1,2}, {2,3}};
     private static int[][] arr2 = {{3,4}, {5,6}};
+
+    //private static int[][] arr1 = {{1}, {2}};
+    //private static int[][] arr2 = {{3}, {4}};
 
     public static void main(String[] args) {
         int[][] solution = {};
         try {
             solution = solution(arr1, arr2);
 
+            StringBuffer sb = new StringBuffer();
+            for ( int row = 0 ; row < solution.length; row++ ){
+                for ( int col = 0; col < solution[row].length; col++ ){
+                    sb.append(solution[row][col]);
+                    if(col != solution[row].length-1) {
+                        sb.append(", ");
+                    }
+                }
+
+                sb.append("\n");
+
+            }
+            System.out.println(sb.toString());
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
-            System.out.println(Arrays.toString(solution));
         }
     }
 
@@ -41,6 +58,11 @@ public class AddProcession {
             throw new Exception();
         }
 
+        for (int row = 0 ; row < answer.length; row++ ){
+            for ( int col = 0 ; col < answer[row].length; col++ ) {
+                answer[row][col] = arr1[row][col] + arr2[row][col];
+            }
+        }
 
         return answer;
     }
